b17c43eb2b30619ee7444d798b21dac176590c32
Nelson Tommogo <nelsontommogo9@gmail.com>
2025-07-03 03:30:55 +0300

    aviator pay

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Aviator Predictor',
-  description: 'Aviator Dector',
+  description: 'Aviator Predictor',
   generator: 'common',
 }
 
